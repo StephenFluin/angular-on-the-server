27dc4e9eb5e6387d229725648506c590a4ad85e0
Stephen Fluin <stephen.fluin@gmail.com>
2017-04-28 12:08:59 -0700

    WIP Server Setup

---
+++ functions/app.server.ts
@@ -0,0 +1,20 @@
+
+import { NgModule } from '@angular/core';
+import { ServerModule } from '@angular/platform-server';
+import { AppComponent } from '../src/app/app.component';
+import { AppModule } from '../src/app/app.module';
+
+
+@NgModule({
+  imports: [
+	  ServerModule,
+	  AppModule
+  ],
+  bootstrap: [
+	  AppComponent
+  ],
+  providers: [
+	//   { provide: NgModuleFactoryLoader, useClass: ServerRouterLoader }
+  ]
+})
+export class AppServerModule {}
--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,22 @@
 var functions = require('firebase-functions');
 
+let renderModuleFactory = require('@angular/platform-server');
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
 });
+
+exports.angular = functions.https.onRequest((req, res) => {
+    response.send(
+        renderModuleFactory({
+            baseUrl: 'http://angular-on-the-server.firebaseapp-staging.com',
+            bootstrap: [AppServerModuleNgFactory]
+        }, {document: '<app-root></app-root>',url: req.url})
+    )
+})
 
 
  exports.reverseString = functions.https.onRequest((req,res) => {
