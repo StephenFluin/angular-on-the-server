6a9522a9000400a2bad3d48fe452308992de14fd
Rob Wormald <robwormald@gmail.com>
2017-04-28 23:37:18 -0700

    function working

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,14 +1,22 @@
+import 'zone.js/dist/zone-node';
+import * as functions from 'firebase-functions';
 import { enableProdMode } from '@angular/core';
 import { renderModuleFactory } from '@angular/platform-server';
 import { AppServerModule } from './app/app.server';
 import { AppServerModuleNgFactory } from './app/app.server.ngfactory';
-import { environment } from './environments/environment';
 
-if (environment.production) {
-  enableProdMode();
+enableProdMode();
+
+function handleRequest(request:functions.Request, response:functions.Response):void {
+  renderModuleFactory(AppServerModuleNgFactory, {url: request.url, document: '<html><body><app-root></app-root></body></html>'})
+  .then(moduleText => {
+    response.send(moduleText);
+  })
+  .catch(err => {
+    response.set('Cache-Control', 'public, max-age=30, s-maxage=60');
+    response.writeHead(500);
+    response.end();
+  })
 }
 
-console.log(AppServerModuleNgFactory)
-
-export { AppServerModuleNgFactory }
-// AppModuleNgFactory
+export const render = functions.https.onRequest(handleRequest);
