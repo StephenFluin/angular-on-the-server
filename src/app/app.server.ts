3f12d211e6e1ec5f47c8f85c6fc1dd2757ba233d
Rob Wormald <robwormald@gmail.com>
2017-04-28 22:21:23 -0700

    make server work

--- src/app/app.server.ts
+++ src/app/app.server.ts
@@ -1,8 +1,8 @@
 
 import { NgModule } from '@angular/core';
 import { ServerModule } from '@angular/platform-server';
-import { AppComponent } from '../src/app/app.component';
-import { AppModule } from '../src/app/app.module';
+import { AppComponent } from './app.component';
+import { AppModule } from './app.module';
 
 
 @NgModule({
--- src/main.server.ts
+++ src/main.server.ts
@@ -1,12 +1,22 @@
+import 'zone.js/dist/zone-node';
+import * as functions from 'firebase-functions';
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { renderModuleFactory } from '@angular/platform-server';
-import { AppModuleNgFactory } from './app/app.module.ngfactory';
-import { environment } from './environments/environment';
+import { AppServerModule } from './app/app.server';
+import { AppServerModuleNgFactory } from './app/app.server.ngfactory';
 
-if (environment.production) {
-  enableProdMode();
+enableProdMode();
+
+function handleRequest(request:functions.Request, response:functions.Response):void {
+  renderModuleFactory(AppServerModuleNgFactory, {url: request.url, document: '<html><body><app-root></app-root></body></html>'})
+  .then(moduleText => {
+    response.send(moduleText);
+  })
+  .catch(err => {
+    response.set('Cache-Control', 'public, max-age=30, s-maxage=60');
+    response.writeHead(500);
+    response.end();
+  })
 }
 
-export { AppModuleNgFactory }
-// AppModuleNgFactory
+export const render = functions.https.onRequest(handleRequest);
